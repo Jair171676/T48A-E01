9560ccd041f5a9320b538fd9edcdcd6e4eaa0f9a
Jair171676 <143846363+Jair171676@users.noreply.github.com>
2025-09-11 19:28:10 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -1,3 +1,4 @@
+import numpy as np
 nombre = "Jair de Jesus Nieto"
 numero_de_matricula = 171676
 fecha = '2025-11-09'
@@ -15,3 +16,11 @@ data = np.array([20100, 24500, 31600, 28400, 49500, 19350, 25600, 30600, 11300, 
     varianza = np.var(data)
     desv_est = np.std(data)
     return (rango, varianza, desv_est)
+
+data = np.array([20100, 24500, 31600, 28400, 49500, 19350, 25600, 30600, 11300, 28560])
+    rango = np.ptp(data)
+    varianza = np.var(data)
+    desv_est = np.std(data)
+    return (rango, varianza, desv_est)
+
+
