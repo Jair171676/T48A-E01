ed480cf84edb4929ec2ae13b50433a51f1cc3b23
Jair171676 <143846363+Jair171676@users.noreply.github.com>
2025-09-11 19:29:47 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -7,7 +7,7 @@ def capitalizacion():
     data = np.array([17, 21, 44, 50, 79, 86, 140, 178, 203])
     media = np.mean(data)
     mediana = np.median(data)
-    moda = stats.mode(data).mode[0]
+    moda = np.mode(data).mode[0]
     desv_est = np.std(data)
     return (media, mediana, moda, desv_est)
 
