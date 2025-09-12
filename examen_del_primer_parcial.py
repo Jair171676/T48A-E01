ce8f5ce1656e2810220b69b04c66db760d444bc2
Jair171676 <143846363+Jair171676@users.noreply.github.com>
2025-09-11 19:22:22 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -1,1 +1,11 @@
+nombre = "Jair de Jesus Nieto"
+numero_de_matricula = 171676
+fecha = '2025-11-09'
 
+def capitalizacion():
+    data = np.array([17, 21, 44, 50, 79, 86, 140, 178, 203])
+    media = np.mean(data)
+    mediana = np.median(data)
+    moda = stats.mode(data).mode[0]
+    desv_est = np.std(data)
+    return (media, mediana, moda, desv_est)
