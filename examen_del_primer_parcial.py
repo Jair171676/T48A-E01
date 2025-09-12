e5d21a28ae5cdcb525283bef87d447e31cff9f4c
Jair171676 <143846363+Jair171676@users.noreply.github.com>
2025-09-12 17:32:15 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 nombre = "Jair de Jesus Nieto"
 numero_de_matricula = 171676
 fecha = '2025-11-09'
@@ -7,9 +8,9 @@ def capitalizacion():
     data = np.array([17, 21, 44, 50, 79, 86, 140, 178, 203])
     media = np.mean(data)
     mediana = np.median(data)
-    #moda = np.mode(data).mode[0]
+    moda = stats.mode(data).mode[0]
     desv_est = np.std(data)
-    return (media, mediana, desv_est)
+    return (media, mediana, moda, desv_est)
 
 def asistencia_dispersion():
     data = np.array([20100, 24500, 31600, 28400, 49500, 19350, 25600, 30600, 11300, 28560])
